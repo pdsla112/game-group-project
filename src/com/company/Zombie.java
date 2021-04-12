b0b043ac05232a1dce487325e67211cac9ced8c2
u6943702 <u6943702@anu.edu.au>
2021-04-12 13:28:38 +1000

    Add random approach method in Zombie class

--- src/com/company/Zombie.java
+++ src/com/company/Zombie.java
@@ -3,11 +3,24 @@ package com.company;
 public class Zombie extends Character {
     private int attackAmount;
     private boolean deathValue;
+    private int approachProb;
+    private boolean willApproach;
 
-    public Zombie(int attackLevel, int attackAmount, boolean deathValue) {
+    public Zombie(int attackLevel, int attackAmount, boolean deathValue, int approachProb) {
         super(attackLevel);
         this.attackAmount = attackAmount;
         this.deathValue = deathValue;
+        this.approachProb = approachProb;
+        this.willApproach = calculateApproach(approachProb);
+    }
+
+    public static boolean calculateApproach(int approachProb) {
+        int value = (int) (Math.random() * 100) + 1;
+
+        if (value <= approachProb)
+            return true;
+        else
+            return false;
     }
 
     public int getAttackAmount() {
