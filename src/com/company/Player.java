2c6e8d2fa13a7f83620f7b342fd27e96d2d5787c
u6943702 <u6943702@anu.edu.au>
2021-04-12 13:08:47 +1000

    Remove hunger parameter from Player class and its methods

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -2,13 +2,11 @@ package com.company;
 
 public class Player {
     private int health;
-    private int hunger;
     private int attackLevel;
     private boolean deathValue;  // True => Player is dead.  False => Player is alive.
 
-    public Player(int health, int hunger, int attackLevel, boolean deathValue) {
+    public Player(int health, int attackLevel, boolean deathValue) {
         this.health = health;
-        this.hunger = hunger;
         this.attackLevel = attackLevel;
         this.deathValue = deathValue;
     }
@@ -21,24 +19,12 @@ public class Player {
         health = health - hp;
     }
 
-    public void eat(int nutrition) {
-        hunger = hunger + nutrition;
-    }
-
-    public void getHungry(int nutritionDeficit) {
-        hunger = hunger - nutritionDeficit;
-    }
-
     public void die() {
         deathValue = true;
     }
 
     public int getHealth() {
         return this.health;
-    }
-
-    public int getHunger() {
-        return this.hunger;
     }
 
     public int getAttackLevel() {
