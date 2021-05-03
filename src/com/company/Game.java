0c677c34bd2d176c0a38db67aac61149d2c41de6
Nayoon Kim <u7079736@anu.edu.au>
2021-05-03 13:27:36 +1000

    forest,hospital

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -76,7 +76,7 @@ public class Game {
     public void setupGameMap() {
         map = new GameMap();
         //setup map
-        Location hospital = new Location("hospital","You are now in the hospital.");
+        Location hospital = new Location("hospital","There is a building that seems to be hospital.");
         map.addLocation(hospital);
         Location cottage = new Location("cottage", "You found a cottage while you were looking for a place to hide, avoiding zombies.");
         map.addLocation(cottage);
@@ -108,7 +108,6 @@ public class Game {
 
 
         //cottage
-
         LevelNode cottageRoot = new LevelNode(null,"Inside a cottage, you saw a glimpse of a child sitting on the cozy sofa in the small living room. She was dozing off, probably because it was warm inside.", null);
         cottage.levelMap = new LevelMap(cottageRoot);
         LevelNode cottageOp1 = new LevelNode("approach","Approach",null);
@@ -136,16 +135,76 @@ public class Game {
         cottage.levelMap.setCompletionNodes(new ArrayList<>(Arrays.asList(cottageOp10,cottageOp11)));
         //player dies
         cottage.levelMap.setDeathNodes(new ArrayList<>(Arrays.asList(cottageOp6)));
-
+        //intro psycho, hunter etc add edges? to not set as an option -> change map and set these nodes and distance = 0?
 
         //forest
-        LevelNode rootForest =  new LevelNode(null,"You are in a forest", null);
+        LevelNode rootForest =  new LevelNode(null,"You are in a forest.", null);
         forest.levelMap = new LevelMap(rootForest);
+        LevelNode forestpsycho = new LevelNode(null,"Walking through the damp, dark road, you realised that there was the old man between the trees.\nHe hasn't seen your side yet.", null);
+        forest.levelMap.setAdjacent(rootForest,new ArrayList<>(Arrays.asList(forestpsycho)));
+        LevelNode forestOp1 = new LevelNode(null,"Approach.",null);
+        LevelNode forestOp2 = new LevelNode(null,"Ignore.",null);
+        forest.levelMap.setAdjacent(forestpsycho,new ArrayList<>(Arrays.asList(forestOp1,forestOp2)));
+        LevelNode forestpsychoapproach = new LevelNode(null,"Wondering why he was in the middle of this forest, you approached him with curiosity and tried to talk to him.\nYou didn't know because he wasn't looking in your direction, but he had dead, lifeless looking eyes with stones in his hands.\nEven before you realise, he looked back at you and started to throw stones towards you.",null);
+        LevelNode forestpsychoignore = new LevelNode(null,"You quickly passed by, knowing that it is abnormal to have a person in the middle of this forest after zombie outbreak.",null);
+        forest.levelMap.setAdjacent(forestOp1,new ArrayList<>(Arrays.asList(forestpsychoapproach)));
+        forest.levelMap.setAdjacent(forestOp2,new ArrayList<>(Arrays.asList(forestpsychoignore)));
+        LevelNode forestpsychowin = new LevelNode(null,"Win.",null);
+        LevelNode forestpsycholose = new LevelNode(null,"Lose.",null);//edit accordingly
+        forest.levelMap.setAdjacent(forestpsychoapproach,new ArrayList<>(Arrays.asList(forestpsychowin,forestpsycholose)));
+        LevelNode forestanimal = new LevelNode(null,"When you heard the rustling sound between the trees, you were stiffened nervously, but soon realised it was an animal that was making the sound.\nGoing closer, you carefully looked through trees and soon noticed that the animal was a (deer).",null); //how to generate different for different animals?
+        forest.levelMap.setAdjacent(forestpsychowin,new ArrayList<>(Arrays.asList(forestanimal)));
+        forest.levelMap.setAdjacent(forestpsychoignore,new ArrayList<>(Arrays.asList(forestanimal)));
+        LevelNode forestOp3 = new LevelNode(null,"Hunt.",null);
+        LevelNode forestOp4 = new LevelNode(null,"Ignore.",null);
+        forest.levelMap.setAdjacent(forestanimal,new ArrayList<>(Arrays.asList(forestOp3,forestOp4)));
+        LevelNode forestanimalhunt = new LevelNode(null,"Having been hungry, you decided to hunt after confirming that you didn't have enough food.",null);
+        LevelNode forestanimalignore = new LevelNode(null,"You decided to pass by as you were not confident enough to hunt it.",null);
+        forest.levelMap.setAdjacent(forestOp3,new ArrayList<>(Arrays.asList(forestanimalhunt)));
+        forest.levelMap.setAdjacent(forestOp4,new ArrayList<>(Arrays.asList(forestanimalignore)));
+        LevelNode forestanimalhuntwin = new LevelNode(null,"You successfully hunt and get meat.",null);
+        LevelNode forestanimalhuntlose = new LevelNode(null,"Hunting failed.",null);
+        //what if hunting fails? what happens
+        forest.levelMap.setAdjacent(forestanimalhunt,new ArrayList<>(Arrays.asList(forestanimalhuntwin,forestanimalhuntlose)));
+        LevelNode forestzombie = new LevelNode(null,"In the middle of the forest, the zombie was finding for the next prey with ears, not eyes perhaps because his eyeballs had rotted.\nFrom zombie's mouth, a liquid suitable to express as filth rather than saliva was falling slowly.\nYour muscles became stiff and bones and joints hardened as if your limbs were binded due to fear.\nOnce you make a big enough sound, the zombie will find you.",null);
+        //depends on approachProb?
+
+
+        forest.levelMap.setCompletionNodes(new ArrayList<>(Arrays.asList(forestpsycho)));
+        forest.levelMap.setDeathNodes(new ArrayList<>(Arrays.asList(forestpsycholose)));
 
         //hospital
-        LevelNode rootHospital = new LevelNode(null,null,null);
+        LevelNode rootHospital = new LevelNode(null,"You are now in the hospital.",null);
         hospital.levelMap = new LevelMap(rootHospital);
+        LevelNode hospitalfirstaid = new LevelNode(null,"While walking in the hallway, you found the first aid kit near the staircase.\nCongratulations, you collected a first aid kit!",null);
+        hospital.levelMap.setAdjacent(rootHospital,new ArrayList<>(Arrays.asList(hospitalfirstaid)));
+        LevelNode hospitalpsycho = new LevelNode(null,"Not wanting to risk yourself taking the lift in a hospital where the electricity supply seemed to be poor, you decided to go by staircase.\nAt third floor, you found someone walking towards you.\n\nHe looks friendly with slightly chubby face, hooded chocolate-brown eyes, round nose and a big smile made by his heart-shaped lips.\nHe realised you coming towards him as well, and he seems to want a conversation with you.",null);
+        LevelNode hospitalOp1 = new LevelNode(null,"Go and talk with him.",null);
+        LevelNode hospitalOp2 = new LevelNode(null, "Ignore.",null);
+        hospital.levelMap.setAdjacent(hospitalfirstaid,new ArrayList<>(Arrays.asList(hospitalpsycho)));
+        hospital.levelMap.setAdjacent(hospitalpsycho,new ArrayList<>(Arrays.asList(hospitalOp1,hospitalOp2)));
+        LevelNode hospitalpsychoapproach = new LevelNode(null,"He was holding a scapel you couldn't see from the distance.\nHe ran towards you so that you failed to escape and got a cut.\n\nYou realised that you are not able to run away, so decided to fight back.",null);
+        LevelNode hospitalpsychoignore = new LevelNode(null,"You thought that it is weird to have person in the middle of the hospital hallway, so you ignored him and went up the staircase quickly so he wouldn't follow you.",null);
+        hospital.levelMap.setAdjacent(hospitalOp1,new ArrayList<>(Arrays.asList(hospitalpsychoapproach)));
+        hospital.levelMap.setAdjacent(hospitalOp2,new ArrayList<>(Arrays.asList(hospitalpsychoignore)));
+        LevelNode hospitalpsychowin = new LevelNode(null,"Won.",null);
+        LevelNode hospitalpsycholose = new LevelNode(null,"Lost.",null);//to be edited once fight decided
+        hospital.levelMap.setAdjacent(hospitalpsychoapproach,new ArrayList<>(Arrays.asList(hospitalpsychowin, hospitalpsycholose)));
+        LevelNode hospitaldoc = new LevelNode(null,"You walked further upstairs and while you walk around fifth floor, you found someone resting in the bed between the curtains in the hospital room.\n\nThe person is lying on the bed, but still awake.\nYou could see the long blonde sticking out, and she was tossing and turning under the duvet.",null);
+        LevelNode hospitalOp3 = new LevelNode(null,"Talk to her.",null);
+        LevelNode hospitalOp4 = new LevelNode(null,"Ignore.",null);
+        hospital.levelMap.setAdjacent(hospitalpsychoignore,new ArrayList<>(Arrays.asList(hospitaldoc)));
+        hospital.levelMap.setAdjacent(hospitalpsychowin,new ArrayList<>(Arrays.asList(hospitaldoc)));
+        hospital.levelMap.setAdjacent(hospitaldoc,new ArrayList<>(Arrays.asList(hospitalOp3,hospitalOp4)));
+        LevelNode hospitaldocapproach = new LevelNode(null,"You decided to talk to her, and after a short conversation, you found out that she was a doctor.\nYou showed her the wounds you had on the way to hospital, asking if she could give you some kind of treatment.\nFortunately, she gladly helped me.",null);
+        LevelNode hospitaldocteam = new LevelNode(null,"You thanked her then explained that you were on the way to get a vaccine, and suggested her to accompany me.\nShe accepted without hesitation, and we went out of the hospital together.",null);//combine? depends on how healAmount works
+        LevelNode hospitaldocignore = new LevelNode(null,"Feeling suspicious, you decided to pass by as if you had seen nothing.",null);
+        hospital.levelMap.setAdjacent(hospitalOp3,new ArrayList<>(Arrays.asList(hospitaldocapproach)));
+        hospital.levelMap.setAdjacent(hospitaldocapproach,new ArrayList<>(Arrays.asList(hospitaldocteam)));
+        hospital.levelMap.setAdjacent(hospitalOp4,new ArrayList<>(Arrays.asList(hospitaldocignore)));
 
+        hospital.levelMap.setCompletionNodes(new ArrayList<>(Arrays.asList(hospitaldocteam,hospitaldocignore)));
+        hospital.levelMap.setDeathNodes(new ArrayList<>(Arrays.asList(hospitalpsycholose)));
 
 //        LevelNode option1 = new LevelNode("go to cottage","Go to cottage",null);
         //new ArrayList<>(Arrays.asList(player.setLocation(cottage),cottage.isVisited())) for actions, error:'void type not allowed here'
@@ -154,10 +213,6 @@ public class Game {
 //        home.levelMap.setAdjacent(root,new ArrayList<>(Arrays.asList(option1,option2)));
         //player.setLocation(cottage); should be one of actions?
         //cottage.levelMap=new LevelMap(home)? current location vs node aka option
-
-
-
-
 
         //continue, DO THIS (NAYOON)
 
