8743b24dd7385648f8ab9aac2c0e6d396254d9ef
u6943702 <u6943702@anu.edu.au>
2021-05-12 10:12:47 +1000

    Convert Level data manipulation to be in the form of an arraylist

--- src/com/company/data/LevelJSON.java
+++ src/com/company/data/LevelJSON.java
@@ -11,30 +11,31 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class LevelJSON {
-    public static void serializeJSON(Level level) {
+    public static void serializeJSON(ArrayList<Level> objectList) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try (FileWriter writer = new FileWriter("LevelDB.json")) {
-            JsonElement tree = gson.toJsonTree(level);
+            JsonElement tree = gson.toJsonTree(objectList);
             gson.toJson(tree, writer);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static Level deserializeJSON() {
-        Level data = null;
+    public static ArrayList<Level> deserializeJSON() {
+        ArrayList<Level> objectList = new ArrayList<>();
         Gson gson = new Gson();
         JsonReader jsonReader = null;
-        final Type objectType = new TypeToken<Level>(){}.getType();
+        final Type objectType = new TypeToken<ArrayList<Level>>(){}.getType();
         try {
             jsonReader = new JsonReader(new FileReader("LevelDB.json"));
-            data = gson.fromJson(jsonReader, objectType);
+            objectList = gson.fromJson(jsonReader, objectType);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return data;
+        return objectList;
     }
 }
