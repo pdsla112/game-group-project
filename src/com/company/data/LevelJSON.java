4b9031cbd5caae3be1dd523755a9d6a14ed5c96f
u6943702 <u6943702@anu.edu.au>
2021-05-12 10:09:13 +1000

    Add functionality to save and retrieve levels for each new game

--- src/com/company/data/LevelJSON.java
+++ src/com/company/data/LevelJSON.java
@@ -1,4 +1,40 @@
 package com.company.data;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+
 public class LevelJSON {
+    public static void serializeJSON(Level level) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (FileWriter writer = new FileWriter("LevelDB.json")) {
+            JsonElement tree = gson.toJsonTree(level);
+            gson.toJson(tree, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Level deserializeJSON() {
+        Level data = null;
+        Gson gson = new Gson();
+        JsonReader jsonReader = null;
+        final Type objectType = new TypeToken<Level>(){}.getType();
+        try {
+            jsonReader = new JsonReader(new FileReader("LevelDB.json"));
+            data = gson.fromJson(jsonReader, objectType);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
 }
