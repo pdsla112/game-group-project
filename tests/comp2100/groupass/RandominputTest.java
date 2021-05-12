b7d030a01209c45be62148d41f4b71c0fb4eadb1
Nayoon Kim <u7079736@anu.edu.au>
2021-05-12 17:39:47 +1000

    tests to be added

---
+++ tests/comp2100/groupass/RandominputTest.java
@@ -0,0 +1,8 @@
+package comp2100.groupass;
+
+import org.junit.Rule;
+
+public class RandominputTest {
+    @Rule
+    public Timeout globalTimeout = Timeout.millis(1000);
+}
---
+++ tests/comp2100/groupass/UserInfoTest.java
@@ -0,0 +1,8 @@
+package comp2100.groupass;
+
+import org.junit.Rule;
+
+public class UserInfoTest {
+    @Rule
+    public Timeout globalTimeout = Timeout.millis(1000);
+}
