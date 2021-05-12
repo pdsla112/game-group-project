9effc6ad949619f7e96143be5a34585e5f5b2b65
u6943702 <u6943702@anu.edu.au>
2021-05-12 15:43:54 +1000

    Add a bloom filter for searching and retrieving saved players

--- src/com/company/BloomFilter/BloomFilter.java
+++ src/com/company/BloomFilter/BloomFilter.java
@@ -3,19 +3,43 @@ package com.company.BloomFilter;
 import java.util.BitSet;
 
 public class BloomFilter {
+    private static final BitSet bitArr = new BitSet(64);
+
     public static void main(String[] args) {
         BitSet bitArray = new BitSet(64);
-        System.out.println(bitArray);
+        add("pdsla112");
+        System.out.println(mightContain("pdsla112"));
+        System.out.println(resetBits());
     }
-//    public static void add(String email) {
-//
-//    }
-//
-//    public static boolean mightContain(String email) {
-//
-//    }
-//
-//    public static int getHash(String email) {
-//        return email.hashCode() % 64;
-//    }
+
+    public static void add(String email) {
+        if (!mightContain(email)) {
+            int hash = getHash(email);
+            bitArr.set(hash, true);
+        }
+    }
+
+    public static boolean mightContain(String email) {
+        int hash = getHash(email);
+        if (bitArr.get(hash))
+            return true;
+        else
+            return false;
+    }
+
+    public static int getHash(String email) {
+        int hash = email.hashCode() % 64;
+        if (hash < 0)
+            return -1 * hash;
+        return hash;
+    }
+
+    public static BitSet getBitArr() {
+        return bitArr;
+    }
+
+    public static BitSet resetBits() {
+        bitArr.set(0, 63, false);
+        return bitArr;
+    }
 }
---
+++ src/com/company/BloomFilter/BloomFilterJSON.java
@@ -0,0 +1,5 @@
+package com.company.BloomFilter;
+
+public class BloomFilterJSON {
+    private BloomFilter bloomFilter;
+}
