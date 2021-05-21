1a2e1113964296b3d20240faf108d010dae4a306
u6943702 <u6943702@anu.edu.au>
2021-05-21 22:36:01 +1000

    Comment out locations package to get rid of error

--- src/com/company/locations/Cottage.java
+++ src/com/company/locations/Cottage.java
@@ -1,32 +1,32 @@
 package com.company.locations;
 
-import com.company.characters.Hunter;
-import com.company.enemies.Psychopath;
-import com.company.items.HuntingKit;
-
-public class Cottage extends Location {
-    private Hunter hunter;
-    private Psychopath psychopath;
-    private HuntingKit huntingKit;
-
-//    public Cottage(boolean visited, Hunter hunter, HuntingKit huntingKit, Psychopath psychopath) {
-//        super(visited);
-//        this.hunter = hunter;
-//        this.huntingKit = huntingKit;
-//        this.psychopath = psychopath;
+//import com.company.characters.Hunter;
+//import com.company.enemies.Psychopath;
+//import com.company.items.HuntingKit;
+//
+//public class Cottage extends Location {
+//    private Hunter hunter;
+//    private Psychopath psychopath;
+//    private HuntingKit huntingKit;
+//
+////    public Cottage(boolean visited, Hunter hunter, HuntingKit huntingKit, Psychopath psychopath) {
+////        super(visited);
+////        this.hunter = hunter;
+////        this.huntingKit = huntingKit;
+////        this.psychopath = psychopath;
+////    }
+////
+////    public Cottage(boolean visited) {
+////        super(visited);
+////    }
+//
+//    //
+//
+//    public Cottage (String name, String description) {
+//        super(name, description);
 //    }
 //
-//    public Cottage(boolean visited) {
-//        super(visited);
+//    public void takeHuntingKit() {
+//        this.huntingKit = null;
 //    }
-
-    //
-
-    public Cottage (String name, String description) {
-        super(name, description);
-    }
-
-    public void takeHuntingKit() {
-        this.huntingKit = null;
-    }
-}
+//}
--- src/com/company/locations/Laboratory.java
+++ src/com/company/locations/Laboratory.java
@@ -1,14 +1,14 @@
 package com.company.locations;
 
-import com.company.locations.Location;
-
-public class Laboratory extends Location {
-
-//    public Laboratory(boolean visited) {
-//        super(visited);
+//import com.company.locations.Location;
+//
+//public class Laboratory extends Location {
+//
+////    public Laboratory(boolean visited) {
+////        super(visited);
+////    }
+//
+//    public Laboratory (String name, String description) {
+//        super(name, description);
 //    }
-
-    public Laboratory (String name, String description) {
-        super(name, description);
-    }
-}
+//}
